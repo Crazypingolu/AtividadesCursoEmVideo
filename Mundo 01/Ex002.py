4b19d841e669e23ddeaf6330d376ecc06e67a3ff
Lucas <lucaswar05@gmail.com>
2024-05-20 09:13:42 -0300

    Primeiro commit de python

---
+++ Mundo 01/Ex002.py
@@ -0,0 +1,10 @@
+'''
+ - Programa: Coletar o nome do usuário e
+ - mostrar uma mensagem de boas-vindas.
+ - Programador: LucasP, Crazypingolu
+ - versão: 1.0
+'''
+# Entrada de dados:
+nome = input('Digite seu nome: ')
+# Saída de dados:
+print(f"Olá! {nome}, muito prazer!")
