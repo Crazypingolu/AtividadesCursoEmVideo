2304bebbb00274ff5692fbc91fbf70f1f527c9cb
Lucas <lucaswar05@gmail.com>
2024-05-22 11:31:56 -0300

    Exercício diário 2205

---
+++ Python/Mundo 01/Ex005.py
@@ -0,0 +1,11 @@
+'''
+ - Programa: Receber um número inteiro e mostrar
+ - seu antecessor e seu sucessor.
+'''
+# Definir variáveis:
+num = int
+# Entrada de dados:
+num = int(input("\nDigite um número (inteiro): "))
+# Processamento e saída de dados:
+print(f"O antecessor do número {num} é {(num - 1)}",
+      f"\nO sucessor do número {num} é {(num + 1)}")
