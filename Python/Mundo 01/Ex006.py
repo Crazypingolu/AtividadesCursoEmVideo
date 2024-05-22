6d559d05911ad6c604dc31f0d9e7259802100dce
Lucas <lucaswar05@gmail.com>
2024-05-22 11:49:19 -0300

    Exercício diário 2205, 2

---
+++ Python/Mundo 01/Ex006.py
@@ -0,0 +1,12 @@
+'''
+ - Programa: Receber um número inteiro e mostrar
+ - seu dobro, triplo e raiz.
+'''
+# Trazer elemento da biblioteca
+from math import sqrt
+# Definir variáveis e receber dados:
+num = int(input("\nDigite um número (int): "))
+# Processamento + saída:
+print(f"O dobro do número {num} é {(num * 2)}", 
+      f"\nO triplo do número {num} é {(num * 3)}", 
+      f"\nA raiz de {num} é {(sqrt(num)):.2f}")
