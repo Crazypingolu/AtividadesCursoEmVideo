0d22d24a6046530c8e3f991374c7a0eca32a5302
Lucas <lucaswar05@gmail.com>
2024-06-08 15:23:17 -0300

    Re-estruturação do Exercício 24.

--- Python/Mundo 01/Ex024.py
+++ Python/Mundo 01/Ex024.py
@@ -1,3 +1,15 @@
-cid = str(input('Digite o nome da cidade: ')).strip().upper()
-st = cid[:5] == 'SANTO'
-print(st)
+'''
+ - Programa: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+ - Programador: LucasP, Crazypingolu
+ - versão: 2.0
+'''
+# Definir variáveis:
+cidade = str
+santo = bool
+# Entrada de dados:
+cidade = input('\nDigite o nome da cidade: ').strip().upper()
+# Processamento de dados:
+santo = cidade[:5] == 'SANTO'
+# Saída de dados:
+print('A cidade tem santo em seu nome? [{}]'.format(santo))
+# Fim
