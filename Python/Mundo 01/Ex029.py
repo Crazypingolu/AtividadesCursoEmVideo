fbf1cf2317a720cd4688fdc8aaea04e0a066677c
Lucas <lucaswar05@gmail.com>
2024-06-08 15:06:23 -0300

    Re-estruturação do Exercício 29.

--- Python/Mundo 01/Ex029.py
+++ Python/Mundo 01/Ex029.py
@@ -1,5 +1,17 @@
+'''
+ - Programa: Escreva um programa que leia a velocidade de um carro.
+ - Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado. 
+ - A multa vai custar R$7,00 por cada Km acima do limite.
+ - Programador: LucasP, Crazypingolu
+ - versão: 1.0
+'''
+#Trazer biblioteca:
 from time import sleep
+# Definir variáveis:
+velo = float
+# Entrada de dados:
 velo = float(input('digite a velocidade do carro (em KM/H): '))
+# Processamento e Saída de dados:
 sleep(1)
 print('Analizando dados')
 sleep(1)
@@ -9,3 +21,4 @@ if velo > 80:
 else:
     print('Você não foi multado. ')
     print('Parabéns, você é um bom motorista. ')
+# Fim
