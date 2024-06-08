8b0ef5cd7aca2a0944d6f5d8cad453a2d9e99193
Lucas <lucaswar05@gmail.com>
2024-06-08 15:16:29 -0300

    Re-estruturação do Exercício 25.

--- Python/Mundo 01/Ex025.py
+++ Python/Mundo 01/Ex025.py
@@ -1,3 +1,15 @@
-nome = str(input('Digite seu nome: ')).strip().upper()
-se = 'SILVA' in nome
-print('seu nome tem silva: \n{}'.format(se))
+'''
+ - Programa: Crie um programa que leia o nome de uma pessoa e diga silva ela tem "SILVA" no nome.
+ - Programador: LucasP, Crazypingolu
+ - versão: 2.0
+'''
+# Definir variáveis:
+nome = str
+silva = bool
+# Entrada de dados:
+nome = input('Digite um NOME (completo): ').strip().upper()
+# Processamento de dados:
+silva = 'SILVA' in nome
+# Saída de dados:
+print('O nome digitado tem silva? [{}]'.format(silva))
+# Fim
