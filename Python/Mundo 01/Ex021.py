05be6927ab429195a6e324bbc7c44ea29799d163
Lucas <lucaswar05@gmail.com>
2024-06-08 15:38:38 -0300

    Re-estruturação do Exercício 21.

--- Python/Mundo 01/Ex021.py
+++ Python/Mundo 01/Ex021.py
@@ -1,5 +1,13 @@
+'''
+ - Programa: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+ - Programador: LucasP, Crazypingolu
+ - versão: 2.0
+'''
+# Trazer biblioteca:
 import pygame
+# Processamento:
 pygame.init()
 pygame.mixer.music.load('Audio B.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
+# Fim.
