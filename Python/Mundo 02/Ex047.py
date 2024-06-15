581c6757b23d9dba86bd03964f8021eeaf156fb5
Lucas <lucaswar05@gmail.com>
2024-06-15 19:03:10 -0300

    Restruturação do Exercício 47

--- Python/Mundo 02/Ex047.py
+++ Python/Mundo 02/Ex047.py
@@ -1,2 +1,8 @@
+'''
+ - Programa: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+ - Programador: LucasP, Crazypingolu
+ - versão: 2.0
+'''
 for c in range(0, 51, 2):
     print('Número {}'.format(c), end=' ')
+# Fim
