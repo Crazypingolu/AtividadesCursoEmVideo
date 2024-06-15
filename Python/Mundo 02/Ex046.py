7167ef23abf394218c24e39d025c7282ba3805f8
Lucas <lucaswar05@gmail.com>
2024-06-15 18:56:55 -0300

    Restruturação do Exercício 46

--- Python/Mundo 02/Ex046.py
+++ Python/Mundo 02/Ex046.py
@@ -1,3 +1,9 @@
+'''
+ - Programa: Faça um programa que mostre na tela uma contagem regressiva 
+ - para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+ - Programador: LucasP, Crazypingolu
+ - versão: 2.0
+'''
 from time import sleep
 import emoji
 for ano_novo in range (10, -1, -1):
