357e2aa4dc23261a150ae519cc2151f38cd0148a
Lucas <lucaswar05@gmail.com>
2024-06-20 15:55:18 -0300

    Reestruturação do exercício 53

--- Python/Mundo 02/Ex053.py
+++ Python/Mundo 02/Ex053.py
@@ -1,16 +1,22 @@
-from time import sleep
+'''
+ - Programa: Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+ - Programador: LucasP, Crazypingolu
+ - versão: 2.0
+'''
+# Definir variáveis:
+frase = str
+fraseInvertida = str = ""
+# Entrada de dados:
 print('')
-print('\033[1;33m{:-^40}\033[m'.format(' Exercício 053 '))
-sleep(0.5)
-print('')
-
-pa = input('Digite uma frase: ').strip().upper()
-pas = pa.split()
-paj = ''.join(pas)
-inver = ''
-for c in range(len(paj)-1, -1, -1):
-    inver += paj[c]
-if inver == paj:
-    print(f'A frase \033[1;34m{paj}\033[m ivertida fica \033[1;31m{inver}\033[m, portanto é um \033[1;33mPALÍNDROMO.\033[m')
+frase = input('Digite uma frase: ').strip().upper()
+# Processamento de dados:
+frase = frase.split()
+frase = ''.join(frase)
+for cto in range(len(frase)-1, -1, -1):
+    fraseInvertida += frase[cto]
+# Saída de dados:
+if fraseInvertida == frase:
+    print(f'A frase \033[1;34m{frase}\033[m ivertida fica \033[1;31m{fraseInvertida}\033[m, portanto é um \033[1;33mPALÍNDROMO.\033[m')
 else:
     print('A frase \033[1;31mNÃO\033[m é um palíndromo.')
+# Fim.
