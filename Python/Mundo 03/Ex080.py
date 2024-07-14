f807db261253ca7178228e29781d4d2a440c8640
Lucas <lucaswar05@gmail.com>
2024-07-14 20:26:58 -0300

    Exercício diário 1407-2

---
+++ Python/Mundo 03/Ex080.py
@@ -0,0 +1,25 @@
+'''
+ - Programa: Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, 
+ - já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela. 
+ - Programador: LucasP, Crazypingolu
+ - versão: 1.0.0
+'''
+# Trazer bibliotecas:
+# Definir variáveis:
+lista = []
+num = int
+cont = int
+# Entrada de dados:
+print(' ')
+for n in range(0,5,1):
+    num = int(input(f"Digite o {n+1}º número: "))
+    if n == 0 or num > lista[len(lista)-1]:
+        lista.append(num)
+    else:
+        cont = 0
+        while cont < len(lista):
+            if num <= lista[cont]:
+                lista.insert(cont, num)
+                break
+            cont += 1
+print(lista)
